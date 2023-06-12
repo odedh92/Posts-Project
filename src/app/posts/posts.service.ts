6a063ab10a20eaaf1154687f70ddfb6d843e01d0
odedh92 <odedh92@gmail.com>
2023-06-12 13:57:23 +0300

    Fixed: Updated image error

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -12,7 +12,7 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) { }
 
   getPosts() {
     this.http
@@ -39,9 +39,12 @@ export class PostsService {
   getPost(id: string) {
     // const post = this.posts.find((p) => p.id === id);
     // return { ...post } as Post;
-    return this.http.get<{ _id: string, title: string, content: string,imagePath:string }>(
-      'http://localhost:3000/api/posts/' + id
-    );
+    return this.http.get<{
+      _id: string;
+      title: string;
+      content: string;
+      imagePath: string;
+    }>('http://localhost:3000/api/posts/' + id);
   }
 
   getPostUpdateListener() {
@@ -74,16 +77,16 @@ export class PostsService {
   }
 
   updatePost(id: string, title: string, content: string, image: File | string) {
-    let postData :Post | FormData;
+    let postData: Post | FormData;
     const post: Post = {
       id: id,
       title: title,
       content: content,
       imagePath: '',
     };
-    if (typeof image === 'object') {
+    if (typeof(image) === 'object') {
       postData = new FormData();
-      postData.append('id', title);
+      postData.append('id', id);
       postData.append('title', title);
       postData.append('content', content);
       postData.append('image', image, title);
@@ -96,16 +99,10 @@ export class PostsService {
       };
     }
     this.http
-      .put('http://localhost:3000/api/posts/' + id, post)
+      .put('http://localhost:3000/api/posts/' + id, postData)
       .subscribe((response) => {
         const updatedPosts = [...this.posts];
         const oldPostIndex = updatedPosts.findIndex((p) => p.id === post.id);
-        const post:Post = {
-          id: id,
-          title: title,
-          content: content,
-          imagePath:'',
-        }
         updatedPosts[oldPostIndex] = post;
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
